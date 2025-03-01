1d68fb9029069ff496a360a6a14b8403426d41b3
Diksha Trivedi <diksha.trivedi2122@gmail.com>
2025-03-01 15:47:27 +0530

    sending name as path

--- src/main/java/com/demo/springconcepts/controller/HelloController.java
+++ src/main/java/com/demo/springconcepts/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController //http://localhost:8080/hello
 public class HelloController {
@@ -13,8 +14,16 @@ public class HelloController {
     }
 
     //used request  mapping to get the query in the form of name and display it
+    //get request using query
     @RequestMapping(value = "/query") //http://localhost:8080/query?name=Diksha
     public String queryController(@RequestParam(value = "name") String name) {
         return "Hello " + name + "!";
     }
+
+    // GET request using a Path Variable
+    @RequestMapping(value = "/param/{name}") // http://localhost:8080/param/Diksha?name=Diksha
+    public String pathVariableController(@PathVariable String name) {
+        return "Hello " + name + " from BridgeLabz!";
+    }
+
 }
